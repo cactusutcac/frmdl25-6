1f473f48be9c9ec5762e200d567c9297cb0d98bd
Yizhen Zang <Y.Zang-3@student.tudelft.nl>
2025-06-06 20:53:04 +0200

    Update `requirements.txt`

--- bdq_encoder/difference.py
+++ bdq_encoder/difference.py
@@ -1,11 +1,5 @@
-# import numpy as np
 import torch
 import torch.nn as nn
-# from torch.utils.data import DataLoader
-# from torchvision import datasets, transforms
-# import matplotlib.pyplot as plt
-# from torchsummary import summary
-# from PIL import Image
 
 class Difference(nn.Module):
     def __init__(self):
